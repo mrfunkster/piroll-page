12a164a6ebd76a3c993fa803628e6e59296c1c27
mr.funkster <mr.funksters@gmail.com>
2020-07-16 13:21:48 +0300

    Fixing active tab script

--- script/tabs.js
+++ script/tabs.js
@@ -1,5 +1,5 @@
-const headerTabs = document.querySelectorAll('.tabs-header-item');
-const contentTabs = document.querySelectorAll('.tabs-content-item');
+const headerTabs    = document.querySelectorAll('.tabs-header-item');
+const contentTabs   = document.querySelectorAll('.tabs-content-item');
 let prevClientWidth = this.outerWidth;
 
 
@@ -16,36 +16,44 @@ for (let headerTab of headerTabs) {
 
         headerTab.classList.add('active-tab');
         const tabID = headerTab.getAttribute('href').substr(1);
-        let tab = document.getElementById(tabID);
+        let tab     = document.getElementById(tabID);
         tab.classList.add('active-tab');
     });
 };
 
 getMaxHeight();
 window.onresize = function (e) {
-    let currentClientWidth = e.target.outerWidth;
+    let currentClientWidth  = e.target.outerWidth;
     if (currentClientWidth != prevClientWidth) {
-        prevClientWidth = currentClientWidth;
+        prevClientWidth     = currentClientWidth;
         getMaxHeight();
     }
 }
 
 function getMaxHeight() {
     let maxTabHeight = 0;
-    contentTabs.forEach((currentTab) => {
-        currentTab.style.height = '';
-    });
+    let currentTab = 0;
+
+    for (let i = 0; i < contentTabs.length; i++) {
+        if (contentTabs[i].classList.contains('active-tab') === true) {
+            currentTab = i;
+        }
+        contentTabs[i].style.height = '';
+    }
+
     for (let i = 0; i < headerTabs.length; i++) {
         headerTabs[i].click();
         let currentHeight = contentTabs[i].offsetHeight;
         if (currentHeight > maxTabHeight) {
-            maxTabHeight = currentHeight;
+            maxTabHeight  = currentHeight;
         }
     }
-    headerTabs[0].click();
+
+    headerTabs[currentTab].click();
     contentTabs.forEach((currentTab) => {
         currentTab.style.height = `${maxTabHeight}px`;
     });
+    
     console.log(`Equal resize tab-content height to ${maxTabHeight}px.`);
 }
 
